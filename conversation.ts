24c8579f748ab7fb8ebea6637a6568d29f7fdc7d
Stephen Chung <spchung95@gmail.com>
2023-12-17 15:12:09 -0500

    private

--- conversation.ts
+++ conversation.ts
@@ -3,8 +3,11 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 export class Conversation {
-  _model!: string;
-  _messages!: { content: string | null; role: OpenAI.ChatCompletionRole }[];
+  private _model!: string;
+  private _messages!: {
+    content: string | null;
+    role: OpenAI.ChatCompletionRole;
+  }[];
 
   constructor(model: string, systemPrompt = "") {
     this._model = model;
@@ -33,10 +36,10 @@ export class Conversation {
     }
 
     // make sure to extend context window with full conversation.
-    this.saveBackAndForth(content, deltas.map((d) => d.content).join());
+    this._saveBackAndForth(content, deltas.map((d) => d.content).join());
   }
 
-  private saveBackAndForth(userContent: string, asstContent: string) {
+  private _saveBackAndForth(userContent: string, asstContent: string) {
     this._messages.push({ role: "user", content: userContent });
     this._messages.push({ role: "assistant", content: asstContent });
   }
